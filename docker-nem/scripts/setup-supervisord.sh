282329b8e5cbbaab57ca7915fff92b8561718cab
rb2nem <info@rb2nem.33mail.com>
2016-03-08 14:26:02 +0100

    corrections after ssh access

--- docker-nem/scripts/setup-supervisord.sh
+++ docker-nem/scripts/setup-supervisord.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/NewEconomyMovement/azure-quickstart-templates/nem-baas-template/docker-nem/files/supervisord.conf.sample >/root/supervisord.conf
+
+home=/root
+mkdir $home/nem/nis -p
+mkdir $home/nem/ncc -p
+curl https://raw.githubusercontent.com/NewEconomyMovement/azure-quickstart-templates/nem-baas-template/docker-nem/files/supervisord.conf >/root/supervisord.conf
+
