afcb35f815b644891c8106d5839ad018fc438c47
rb2nem <info@rb2nem.33mail.com>
2016-03-12 17:38:27 +0100

    persistent storage

--- docker-nem/scripts/setup-docker-nem.sh
+++ docker-nem/scripts/setup-docker-nem.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
+# setup data disk.
+# from docker-neo4j template
 
-# save stuff in /root, ok as docker engine runs as root
-home=/root
+# create a partition table for the disk
+parted -s /dev/sdc mklabel msdos
+
+# create a single large partition
+parted -s /dev/sdc mkpart primary ext4 0\% 100\%
+
+# install the file system
+mkfs.ext4 /dev/sdc1
+
+# create the mount point
+mkdir /datadisk
+
+# mount the disk
+sudo mount /dev/sdc1 /datadisk/
+
+# add mount to /etc/fstab to persist across reboots
+echo "/dev/sdc1    /datadisk/    ext4    defaults 0 0" >> /etc/fstab
+
+
+
+# save stuff in /datadisk
+home=/datadisk
 
 
 # create directories needed by nem container
@@ -10,15 +32,13 @@ mkdir $home/nem/nis -p
 mkdir $home/nem/ncc -p
 
 # get supervisord config
-curl https://raw.githubusercontent.com/NewEconomyMovement/azure-quickstart-templates/nem-baas-template/docker-nem/files/supervisord.conf >/root/supervisord.conf
+curl https://raw.githubusercontent.com/NewEconomyMovement/azure-quickstart-templates/nem-baas-template/docker-nem/files/supervisord.conf >$home/supervisord.conf
 
 key=$(< /dev/urandom tr -dc a-f0-9 | head -c64)
 name=nemonazure_$(< /dev/urandom tr -dc a-z | head -c 20)
 
 # generate random name and bootkey
-cat >/root/nis.config-user.properties <<EOF
+cat > $home/nis.config-user.properties <<EOF
 nis.bootName = $name
 nis.bootKey = $key
 EOF
-
-
